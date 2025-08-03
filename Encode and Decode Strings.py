f45cca69e88beade5183cf314c8ce3686f2d048f
IshaqNiloy <ishaqniloy1996@gamil.com>
2025-08-03 13:09:09 +0600

    first commit

---
+++ Encode and Decode Strings.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ''
+
+        for item in strs:
+            encoded_string += item + '€'
+
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        return s.split('€')[:-1]
+
+if __name__ == '__main__':
+    input = ["neet","code","love","you"]
+    obj = Solution()
+    encoded_string = obj.encode(input)
+    print(obj.decode(encoded_string))
